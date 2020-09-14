d2a97faa4c597fe3f65fea0b4b41e122dad71d06
unknown <agustinclasst@gmail.com>
2020-09-13 21:24:16 -0300

    Tp terminado

---
+++ tp_laboratorio_2/Entidades/Calculadora.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public static class Calculadora
+    {
+        private static string ValidarOperador(char operador)
+        {
+            string operadorValidado;
+
+            if (operador == '*')
+            {
+                operadorValidado = "*";
+
+            }else if(operador == '/')
+            {
+                operadorValidado = "/";
+
+            }
+            else if (operador == '-')
+            {
+                operadorValidado = "-";
+
+            }
+            else
+            {
+                operadorValidado = "+";
+            }
+
+            return operadorValidado;
+        }
+
+        public static double Operar(Numero num1, Numero num2, string operador)
+        {
+            double resultado = 0;
+
+            StringBuilder operadorAux = new StringBuilder(operador);
+            operador = ValidarOperador(operadorAux[0]);
+
+            switch (operador)
+            {
+                case "+":
+                    resultado = num1 + num2;
+                    break;
+                case "-":
+                    resultado = num1 - num2;
+                    break;
+                case "*":
+                    resultado = num1 * num2;
+                    break;
+                case "/":
+                    resultado = num1 / num2;
+                    break;
+            }
+            
+            return resultado;
+        }
+    }
+}
